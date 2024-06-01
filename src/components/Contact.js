afc0b15dc23cfe5c03620ec4834ceec1b699594c
spyer <sundayomoladee11@gmail.com>
2024-06-01 10:56:34 +0100

    contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import './contact.css'
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contactContainer'>
+    <h1 className='contacttext'>Contact Us</h1>
+    <p className='contacttext'>We are available to help you with your giftcard verification needs and to provide you support with a quick solution to your problem.</p>
+    <section className='innercontactcontainer'>
+     <form action="" className='helpForm'>
+      <label htmlFor="email">Email</label>
+      <input type="email" required id='email' className='email'/>
+      <label htmlFor="message">Message</label>
+      <textarea name="" required id="message" placeholder='Message' className='message' cols="30" rows="10"></textarea>
+      <button type='submit' className='messagebtn' >Submit</button>
+     </form>
+    </section>
+  </div>
   )
 }
 
